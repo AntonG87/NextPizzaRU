cfaa16e84bed447118e6d45666cc226c30325c0d
Anton Gruzdev <tauit895031@gmail.com>
2025-05-07 12:42:59 +0300

    FINAL UPDATE

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+const arr = [1, 5, 3, 9, 2];
+let max = arr[0];
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+console.log(max); // 9
